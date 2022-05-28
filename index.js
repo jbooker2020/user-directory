7a4fbb93489fb351347546c90e783ecbf786cbb8
Jordan Booker <jordanlbooker2020@gmail.com>
2022-05-28 18:55:40 -0500

    change the text of h1elements with buttons

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ function changeText(ev) {
 }
 
 button.addEventListener('click', changeText)
+
+
+const second_button = document.querySelector('#heady')
+
+function changeheader() {
+    const header = document.querySelector('h1')
+    header.textContent = 'I do the dash and I pull off'
+}
+
+second_button.addEventListener('click', changeheader)
+
+const third_button = document.querySelector('button#lasty')
+
+function change2ndHeader() {
+    const header2 = document.querySelector('h1#mortal')
+    header2.textContent = 'hello Drake'
+}
+
+third_button.addEventListener('click', change2ndHeader)
