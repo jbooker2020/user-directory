30f40175bcbf2efc73ffe3efccc117fdf048fb68
Jordan Booker <jordanlbooker2020@gmail.com>
2022-05-28 20:11:25 -0500

    Update the heading with a value from the form

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
+// This variable grabs the HTML Element with the h1 id  'userForm'
+const form = document.querySelector('#userForm')
 
-const button = document.querySelector('button.greeting')
-
-function changeText(ev) {
-   ev.target.textContent = 'clickity click'
+// This function changes the h1 element to whatever a user types in the input box
+const handleSubmit = function(ev) {
+    ev.preventDefault()
+    const heading = document.querySelector('h1')
+    const f = ev.target
+    const username = f.userName.value
+    heading.textContent = username
+    
 }
 
-button.addEventListener('click', changeText)
-
-
-const second_button = document.querySelector('#heady')
-
-// Calling a function as a variable must be created before it is ever used
-const changeheader = function() {
-    const header = document.querySelector('h1')
-    header.textContent = 'I do the dash and I pull off'
-}
-
-second_button.addEventListener('click', changeheader)
-
-const third_button = document.querySelector('button#lasty')
-
-function change2ndHeader() {
-    const header2 = document.querySelector('h1#mortal')
-    header2.textContent = 'hello Drake'
-}
-
-third_button.addEventListener('click', change2ndHeader)
+// This allows the user to submit their input and have the handleSubmit function to perform
+form.addEventListener('submit', handleSubmit)
