08354cec0b1f91edd25940b622346eea40d13aa5
Jordan Booker <jordanlbooker2020@gmail.com>
2022-05-31 15:59:02 -0500

    Extract renderColor function

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const form = document.querySelector('#userForm')
 
+const renderColor = function(color) {
+    const colorDiv = document.createElement('div')
+    colorDiv.style.backgroundColor = color
+    colorDiv.style.width = '6rem'
+    colorDiv.style.height = '3rem'
+    return colorDiv
+}
+
 
 const handleSubmit = function(ev) {
     // This chunk of code gets values out of the form
@@ -11,6 +19,10 @@ const handleSubmit = function(ev) {
 
     
     // This chunk of code creates list items
+    //const renderListItem = function() {
+
+    //}
+
     const nameItem = document.createElement('li')
     nameItem.textContent = `Name: ${username}`
 
@@ -19,22 +31,25 @@ const handleSubmit = function(ev) {
 
     
 
-    // This chunk of code creats a div that shows the color selected by a user
-    const colorDiv = document.createElement('div')
-    colorDiv.style.backgroundColor = favoritecolor
-    colorDiv.style.width = '6rem'
-    colorDiv.style.height = '3rem'
-
     // This chunk of code creates the color list items and appends the div to the list
     const colorItem = document.createElement('li')
     colorItem.textContent = 'Favorite Color: '
-    colorItem.appendChild(colorDiv)
+    colorItem.appendChild(renderColor(favoritecolor))
     
     // This chunk of code builds the actual list
-    const list = document.createElement('ul')
+    const renderList = function() {
+        const list = document.createElement('ul')
+        return list
+   
+    }
+
+    list = renderList()
+
     list.appendChild(nameItem)
     list.appendChild(ageItem)
     list.appendChild(colorItem)
+    
+    
 
     // This chunk of code sticks the code on the page
     const users = document.querySelector('#users')
@@ -45,6 +60,5 @@ const handleSubmit = function(ev) {
     f.userYame.focus()
     
 }
-
 
 form.addEventListener('submit', handleSubmit)
